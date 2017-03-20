867ce1b0b85a1389fc9acf0cb9a4b1c4be49ea88
Nandesh <nandesh@cloudcoreo.com>
2017-03-20 11:57:00 -0700

    test

--- services/config.rb
+++ services/config.rb
@@ -1,12 +1,12 @@
 coreo_aws_rule "cloudtrail-logs-encrypted-rule" do
   action :define
   service :user
-  category "Audit"
+  category "Manual"
   link "http://kb.cloudcoreo.com/"
   display_name "Ensure CloudTrail logs are encrypted at rest using KMS CMKs (Scored)"
   suggested_action "It is recommended that CloudTrail be configured to use SSE-KMS."
   description "AWS CloudTrail is a web service that records AWS API calls for an account and makes those logs available to users and resources in accordance with IAM policies. AWS Key Management Service (KMS) is a managed service that helps create and control the encryption keys used to encrypt account data, and uses Hardware Security Modules (HSMs) to protect the security of encryption keys. CloudTrail logs can be configured to leverage server side encryption (SSE) and KMS customer created master keys (CMK) to further protect CloudTrail logs."
-  level "Warning"
+  level "Manual"
   meta_cis_id "2.7"
   meta_cis_scored "true"
   meta_cis_level "2"
